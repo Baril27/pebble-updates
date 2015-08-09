87a01edd6fc2256e686d7840d09f5e1c9f443973
Eric Baril <Baril27@users.noreply.github.com>
2015-08-09 01:11:31 -0700

    Add initial source js for basic functionality

---
+++ src/app.js
@@ -0,0 +1,73 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+var Vector2 = require('vector2');
+var ajax = require('ajax');
+
+var main = new UI.Card({
+  title: 'Pebble.js',
+  icon: 'images/menu_icon.png',
+  subtitle: 'Hello World!',
+  body: 'Press any button.'
+});
+
+main.show();
+
+main.on('click', 'up', function(e) {
+  var menu = new UI.Menu({
+    sections: [{
+      items: [{
+        title: 'Pebble.js',
+        icon: 'images/menu_icon.png',
+        subtitle: 'Can do Menus'
+      }, {
+        title: 'Second Item',
+        subtitle: 'Subtitle Text'
+      }]
+    }]
+  });
+  menu.on('select', function(e) {
+    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+    console.log('The item is titled "' + e.item.title + '"');
+  });
+  menu.show();
+});
+
+main.on('click', 'select', function(e) {
+  var wind = new UI.Window({
+    fullscreen: true,
+  });
+  var textfield = new UI.Text({
+    position: new Vector2(0, 65),
+    size: new Vector2(144, 30),
+    font: 'gothic-24-bold',
+    text: 'Text Anywhere!',
+    textAlign: 'center'
+  });
+  wind.add(textfield);
+  wind.show();
+});
+
+main.on('click', 'down', function(e) {
+  var card = new UI.Card();
+  card.title('The quote of the day is:');
+
+  ajax(
+    {
+      url: 'http://api.theysaidso.com/qod.json',
+      type: 'json'
+    },
+    function(data, status, request) {
+      card.body('Quote of the day is: ' + data.contents.quotes[0].quote);
+    },
+    function(error, status, request) {
+      card.body('The ajax request failed: ' + error);
+    }
+  );
+
+  card.show();
+});
